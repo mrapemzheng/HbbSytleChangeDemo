e4104bdbdd500791c0e511f477b22d6f1aab57c9
apem <apple@appledemacbook-air.local>
2015-11-04 22:04:19 +0800

    commit

--- HbbSytleChangeDemo/ViewController.m
+++ HbbSytleChangeDemo/ViewController.m
@@ -8,8 +8,10 @@
 
 #import "ViewController.h"
 #import "S_MainColorSwitch.h"
+#import "HbbUIStyleHelper.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *thirdLabel; //普通视图或者第三方视图
 
 @end
 
@@ -20,6 +22,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     S_MainColorSwitch *colorSwitch = [[S_MainColorSwitch alloc] initWithFrame:CGRectMake(50, 100, 80, 30)];
+    colorSwitch.on = YES;
     [self.view addSubview:colorSwitch];
     [colorSwitch hbbStyleControlChangeStyle];
 }
@@ -34,9 +37,33 @@
 {
     [super hbbStyleControlChangeStyle];
     
-//    UIColor *c = [HbbUIStyleHelper colorWithKey:@""];
-//    self.colorSwitch.bintcolor = c;
+    UIColor *color = [HbbUIStyleHelper colorWithKey:kHbbUIStyleColorStyleLineColor];
+    NSInteger fontSize = [HbbUIStyleHelper fontSizeWithKey:kHbbUIStyleFontSizeStyleBigestFontSize];
+    self.thirdLabel.textColor = color;
+    self.thirdLabel.font = [UIFont systemFontOfSize:fontSize];
+    
     
 }
+
+#pragma mark - IBAction
+
+//体验换肤
+- (IBAction)experienceChangeStyle:(UIButton *)sender {
+    
+    sender.selected = !sender.selected;
+    //读取样式文件
+    NSString *styleFilePath;
+    if (sender.selected == YES) {
+       styleFilePath = [[NSBundle mainBundle] pathForResource:@"MainUIStyle2.plist" ofType:nil];
+        
+    } else {
+        styleFilePath = [[NSBundle mainBundle] pathForResource:@"MainUIStyle.plist" ofType:nil];
+    }
+    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:styleFilePath];
+    [[HbbUIStyleHelper shared] changeStyleWithStyleDictionary:dict];
+    
+    
+}
+
 
 @end
